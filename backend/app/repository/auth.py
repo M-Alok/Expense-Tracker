d05c62de0f269a219ec1ebc66735188048f5cb71
Alok <ams03022001@gmail.com>
2025-10-21 16:22:07 +0530

    Chnaging passlib algorithm for password hashing

--- backend/app/repository/auth.py
+++ backend/app/repository/auth.py
@@ -3,30 +3,38 @@ from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from sqlalchemy.orm import Session
 from jose import JWTError, jwt
-from bcrypt import gensalt, hashpw, checkpw
 from app.database import get_db
 from app.models import User
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
 
-SECRET_KEY = "mysecretkey123"
-ALGORITHM = "HS256"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = 7 * 24 * 60
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def verify_password(plain_password, hashed_password):
-    return checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def get_password_hash(password):
-    salt = gensalt()
-    hashed_password = hashpw(password.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')
+    """Hash a password for storing in the database."""
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    """Verify a plain password against the hashed password."""
+    return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict):
+    """Create a JWT token for a user."""
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    """Get the current user from the JWT token."""
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
